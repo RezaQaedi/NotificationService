cde1b3ae0c01ab4762f5e81d253bb1f0d8600323
rezaghaedi <rezagh425@gmail.com>
2025-11-24 17:37:29 +0330

    Add resolver impl

---
+++ NotificationService/Domain/Notifications/NotificationManagerResolver.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Options;
+using NotificationService.Domain.Options;
+
+namespace NotificationService.Domain.Notifications
+{
+    public class NotificationManagerResolver(IServiceProvider serviceProvider, 
+        IOptions<NotificationServiceOptions> options) : INotificationManagerResolver
+    {
+        public INotificationManager Resolve(string notificationMethod)
+        {
+            return (INotificationManager)serviceProvider.GetRequiredService(
+                options.Value.Providers[notificationMethod].NotificationManagerType);
+        }
+    }
+}

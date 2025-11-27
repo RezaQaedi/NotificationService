e3271b66f12adef7e7093d8ad3e9abea749278bb
rezaghaedi <rezagh425@gmail.com>
2025-11-27 19:03:59 +0330

    Add place holder for push notif

---
+++ NotificationService/Providers/PushNotification/PushNotificaitonManager.cs
@@ -0,0 +1,16 @@
+﻿using NotificationService.Domain.Notifications;
+using NotificationService.Domain.Shared;
+
+namespace NotificationService.Providers.PushNotification
+{
+    public class PushNotificationManager : NotificationManagerBase, INotificationManager
+    {
+        protected override string MethodName { get; } = "PushNotification";
+
+        public override Task<Notification> CreateAsync(CreateNotificationModel model, CancellationToken cancellationToken = default)
+        {
+            // Todo 
+            throw new NotImplementedException();
+        }
+    }
+}
